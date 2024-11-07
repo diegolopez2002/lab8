1c24e59bb1c3c2809a43409da403a03b6768c974
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-11-07 16:49:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,37 +5,56 @@ var height = +svg.attr('height');
 var colorScale = d3.scaleOrdinal(d3.schemeTableau10);
 var linkScale = d3.scaleSqrt().range([1,5]);
 
-var simulation = d3.forceSimulation()
-    .force('link', d3.forceLink().id(function(d) { return d.id; }))
-    .force('charge', d3.forceManyBody())
-    .force('center', d3.forceCenter(width / 2, height / 2));
 
-simulation.nodes(dataset.nodes)
-    .on('tick', tickSimulation);
+var drag = d3.drag()
+    .on('start', dragstarted)
+    .on('drag', dragged)
+    .on('end', dragended);
 
-simulation.force('link')
-    .links(dataset.links);
+d3.json('les_miserables.json').then(function(dataset) {
+    console.log(dataset);
+    network = dataset;
+    linkScale.domain(d3.extent(network.links, function(d){ return d.value;}));
 
-var linkEnter = linkG.selectAll('.link')
-    .data(network.links)
-    .enter()
-    .append('line')
-    .attr('class', 'link')
-    .attr('stroke-width', function(d) {
-        return linkScale(d.value);
-    });
+    var linkG = svg.append('g')
+        .attr('class', 'links-group');
 
-var nodeEnter = nodeG.selectAll('.node')
-    .data(network.nodes)
-    .enter()
-    .append('circle')
-    .attr('class', 'node')
-    .attr('r', 6)
-    .style('fill', function(d) {
-        return colorScale(d.group);
-    });
+    var nodeG = svg.append('g')
+        .attr('class', 'nodes-group');
 
+    var simulation = d3.forceSimulation()
+        .force('link', d3.forceLink().id(function(d) { return d.id; }))
+        .force('charge', d3.forceManyBody())
+        .force('center', d3.forceCenter(width / 2, height / 2));
+    
+    simulation.nodes(dataset.nodes)
+        .on('tick', tickSimulation);
+    
+    simulation.force('link')
+        .links(dataset.links);
+
+    var linkEnter = linkG.selectAll('.link')
+        .data(network.links)
+        .enter()
+        .append('line')
+        .attr('class', 'link')
+        .attr('stroke-width', function(d) {
+            return linkScale(d.value);
+        });
+
+    var nodeEnter = nodeG.selectAll('.node')
+        .data(network.nodes)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('r', 6)
+        .style('fill', function(d) {
+            return colorScale(d.group);
+        });
     nodeEnter.call(drag);
+    
+
+});
 
 function tickSimulation() {
     linkEnter
@@ -48,25 +67,6 @@ function tickSimulation() {
         .attr('cx', function(d) { return d.x;})
         .attr('cy', function(d) { return d.y;});
 }
-
-var drag = d3.drag()
-    .on('start', dragstarted)
-    .on('drag', dragged)
-    .on('end', dragended);
-
-d3.json('les_miserables.json').then(function(dataset) {
-    console.log(dataset);
-    network = dataset;
-
-    linkScale.domain(d3.extent(network.links, function(d){ return d.value;}));
-
-    var linkG = svg.append('g')
-        .attr('class', 'links-group');
-
-    var nodeG = svg.append('g')
-        .attr('class', 'nodes-group');
-
-});
 
 function dragstarted(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
